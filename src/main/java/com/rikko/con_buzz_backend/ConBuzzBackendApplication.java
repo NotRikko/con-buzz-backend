8f6b1555da9434ad92851acb84301257083a8a4e
Rikko <bulletbill51@yahoo.com>
2026-04-21 12:27:12 -0700

    Initial commit backend

---
+++ src/main/java/com/rikko/con_buzz_backend/ConBuzzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rikko.con_buzz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConBuzzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConBuzzBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rikko/con_buzz_backend/ConBuzzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rikko.con_buzz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConBuzzBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
